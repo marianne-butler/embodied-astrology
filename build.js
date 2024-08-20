e1c4971548c2d1d604ae83f30adf646d88cba258
Marianne <marianne.butler@gmail.com>
2024-08-20 12:17:30 +0100

    simplify

--- build.js
+++ build.js
@@ -1,9 +1,3 @@
-import fs from 'fs';
-import {Notion} from './sessions.js';
-import path, { dirname } from 'node:path';
-
-const notionParams = {}; // tag
-
-const sessions = await Notion(notionParams);
+const fs = require('fs');
 
 fs.writeFileSync("public/data.json", JSON.stringify({test:"ok"}));
