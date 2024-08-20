c73774e60f81791e3d325509a6ea0055bf4eeb1c
Marianne <marianne.butler@gmail.com>
2024-08-20 14:40:54 +0100

    try again

--- netlify/functions/playground.js
+++ netlify/functions/playground.js
@@ -1,28 +1,28 @@
-const stytch = require('@stytch');
+// const stytch = require('@stytch');
 
-const { STYTCH_TEST_PROJECT_ID, STYTCH_TEST_SECRET, STYTCH_TEST_TOKEN } = process.env;
+// const { STYTCH_TEST_PROJECT_ID, STYTCH_TEST_SECRET, STYTCH_TEST_TOKEN } = process.env;
 
-let response, error;
+// let response, error;
 
-const client = new stytch.Client({
-  env:'TEST',
-  project_id: STYTCH_TEST_PROJECT_ID,
-  secret: STYTCH_TEST_SECRET,
-});
+// const client = new stytch.Client({
+//   env:'TEST',
+//   project_id: STYTCH_TEST_PROJECT_ID,
+//   secret: STYTCH_TEST_SECRET,
+// });
 
-exports.handler = async function (event, context) {
-  client.users.create({
-    email: "marianne.voidofcourse@gmail.com",
-  })
-    .then(resp => { response = resp; })
-    .catch(err => { error = err; });
+// exports.handler = async function (event, context) {
+//   client.users.create({
+//     email: "marianne.voidofcourse@gmail.com",
+//   })
+//     .then(resp => { response = resp; })
+//     .catch(err => { error = err; });
 
-   return {
-       statusCode: 200,
-       body: JSON.stringify({"result": response, "error": error}),
-       headers: {
-         "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-       }, 
-   };
+//    return {
+//        statusCode: 200,
+//        body: JSON.stringify({"result": response, "error": error}),
+//        headers: {
+//          "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+//        }, 
+//    };
 
-};
+// };
