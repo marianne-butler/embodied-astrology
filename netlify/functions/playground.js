1c6e69caf7a56978bc6dd14e6ed5073a3c6a8d9f
Marianne <marianne.butler@gmail.com>
2024-08-21 07:32:48 +0100

    update stytch v

--- netlify/functions/playground.js
+++ netlify/functions/playground.js
@@ -4,52 +4,33 @@ const token = 'public-token-test-426746fe-aad7-4abd-8733-3b9a68c375fa',
 sectret = 'project-test-6f387723-84a0-4c92-8f67-4f1b259d9ba0',
 id = 'secret-test-zgxGcqbRPjKEr_MkhBWag2-KSXvaY1MoCKY';
 
+const client = new stytch.Client({
+	project_id: id,
+	secret: secret,
+});
+
 exports.handler = async function (event, context) {
-  try {
-    const client = new stytch.Client({
-      project_id: id,
-      secret: secret,
-    });
+	const params = {
+		email: "marianne.voidofcourse@gmail.com",
+	};
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({"result":"ok"}),
-        headers: {
-          "Access-Control-Allow-Origin" : "*",
-        }, 
-      }; 
-  } catch (e) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({"error":e}),
-      headers: {
-        "Access-Control-Allow-Origin" : "*",
-      }, 
-    }; 
-  }
-  
-
-  const params = {
-    email: "marianne.voidofcourse@gmail.com",
-  };
-
-  client.users.create(params)
-    .then(resp => { 
-      return {
-        statusCode: 200,
-        body: JSON.stringify({"result":"ok"}),
-        headers: {
-          "Access-Control-Allow-Origin" : "*", 
-        }, 
-      }; 
-    })
-   .catch(err => { 
-    return {
-      statusCode: 500,
-      body: JSON.stringify({"error":err}),
-      headers: {
-        "Access-Control-Allow-Origin" : "*",
-      }, 
-    }; 
-  });
+  	client.users.create(params)
+    	.then(resp => { 
+	      	return {
+	        	statusCode: 200,
+	        	body: JSON.stringify({"result":resp}),
+	        	headers: {
+	          		"Access-Control-Allow-Origin" : "*", 
+	        	}, 
+	      	}; 
+    	})
+	   .catch(err => { 
+		    return {
+			    statusCode: 500,
+			    body: JSON.stringify({"error":err}),
+			    headers: {
+			    	"Access-Control-Allow-Origin" : "*",
+			   	}, 
+		   	}; 	
+		});
 };
