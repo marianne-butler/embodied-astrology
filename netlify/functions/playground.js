973c24e01f8ae5765b52f49769b2ce8073c4820b
Marianne <marianne.butler@gmail.com>
2024-08-21 13:21:33 +0100

    playground

--- netlify/functions/playground.js
+++ netlify/functions/playground.js
@@ -5,9 +5,24 @@ secret = 'secret-test-zgxGcqbRPjKEr_MkhBWag2-KSXvaY1MoCKY=',
 id = 'project-test-6f387723-84a0-4c92-8f67-4f1b259d9ba0';
 
 exports.handler = async function (event, context) {
-	try {
-		let isError, response;
+	let error, response;
+	function composeResponse() {
+		return {
+        	statusCode: 200,
+        	body: JSON.stringify(response),
+        	headers: { "Access-Control-Allow-Origin" : "*" }
+      	}
+	}
 
+	function composeError() {
+		return {
+        	statusCode: 500,
+        	body: JSON.stringify(error),
+        	headers: { "Access-Control-Allow-Origin" : "*" }
+      	}
+	}
+
+	try {
 		const client = new stytch.Client({
 			project_id: id,
 			secret: secret,
@@ -15,40 +30,26 @@ exports.handler = async function (event, context) {
 		});
 
 		switch (event.queryStringParameters.action) {
-			case "GET_USER":
+			case "PROFILE":
 				await client.users.get({user_id: event.queryStringParameters.user_id})
-				    .then(resp => {
-				    	response = resp;
-				    })
-				    .catch(err => {
-				    	response = err; 
-				    	isError = true;
-				    });
-
-	    	    return {
-	            	statusCode: isError ? 500 : 200,
-	            	body: JSON.stringify(response),
-	            	headers: { "Access-Control-Allow-Origin" : "*" }
-	          	};
-
+				    .then(resp => response = resp)
+				    .catch(err => error = err);
+	    	    return error == null ? composeResponse() : composeError();
 				break;
-			
-			default: 
-			    return {
-		        	statusCode: 500,
-		        	body: JSON.stringify({"error": "action not handled"}),
-		        	headers: { "Access-Control-Allow-Origin" : "*" }
-		      	};
+			case "MAGIC":
+				await client.magicLinks.email.loginOrCreate({email: event.queryStringParameters.email})
+					.then(resp => response = resp)
+				  	.catch(err => error = err);
+				return error == null ? composeResponse() : composeError();
+				break;
+			default:
+				error = "action not handled";
+			    return composeError();
 				break;
 		}
 	}
 	catch (clientErr) {
-		 return {
-		    statusCode: 500,
-		    body: JSON.stringify({"error": clientErr}),
-		    headers: {
-		    	"Access-Control-Allow-Origin" : "*",
-		   	}, 
-	   	}; 	
+		clientErr = clientErr;
+		return composeError();
 	}
 };
