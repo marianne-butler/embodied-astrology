f1f3509ddcd0b4336161a82d1324d892cd196511
Marianne <marianne.butler@gmail.com>
2024-08-21 07:34:28 +0100

    update stytch v

--- netlify/functions/playground.js
+++ netlify/functions/playground.js
@@ -1,7 +1,7 @@
 const stytch = require('stytch');
 
 const token = 'public-token-test-426746fe-aad7-4abd-8733-3b9a68c375fa',
-sectret = 'project-test-6f387723-84a0-4c92-8f67-4f1b259d9ba0',
+secret = 'project-test-6f387723-84a0-4c92-8f67-4f1b259d9ba0',
 id = 'secret-test-zgxGcqbRPjKEr_MkhBWag2-KSXvaY1MoCKY';
 
 const client = new stytch.Client({
