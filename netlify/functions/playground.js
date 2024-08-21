8000ac12c1c76a93e2674b16559970c0a5ef894b
Marianne <marianne.butler@gmail.com>
2024-08-21 07:16:08 +0100

    try the sdk again...

---
+++ netlify/functions/playground.js
@@ -0,0 +1,56 @@
+const stytch = require('stytch');
+
+const token = 'public-token-test-426746fe-aad7-4abd-8733-3b9a68c375fa',
+sectret = '',
+id = '';
+
+exports.handler = async function (event, context) {
+  try {
+    const client = new stytch.Client({
+      env: "test",
+      project_id: 'project-test-6f387723-84a0-4c92-8f67-4f1b259d9ba0',
+      secret: 'secret-test-zgxGcqbRPjKEr_MkhBWag2-KSXvaY1MoCKY',
+    });
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({"result":"ok"}),
+        headers: {
+          "Access-Control-Allow-Origin" : "*",
+        }, 
+      }; 
+  } catch (e) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({"error":e}),
+      headers: {
+        "Access-Control-Allow-Origin" : "*",
+      }, 
+    }; 
+  }
+  
+
+  const params = {
+    email: "marianne.voidofcourse@gmail.com",
+  };
+
+  client.users.create(params)
+    .then(resp => { 
+      return {
+        statusCode: 200,
+        body: JSON.stringify({"result":"ok"}),
+        headers: {
+          "Access-Control-Allow-Origin" : "*", 
+        }, 
+      }; 
+    })
+   .catch(err => { 
+    return {
+      statusCode: 500,
+      body: JSON.stringify({"error":err}),
+      headers: {
+        "Access-Control-Allow-Origin" : "*",
+      }, 
+    }; 
+  });
+};
