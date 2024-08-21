6711c75b654ba7b8ee51da7ac4b1b7ea6f567f19
Marianne <marianne.butler@gmail.com>
2024-08-21 18:27:01 +0100

    user id

--- netlify/functions/playground.js
+++ netlify/functions/playground.js
@@ -1,4 +1,5 @@
 const stytch = require('stytch');
+const fetch = require('node-fetch');
 
 const token = 'public-token-test-426746fe-aad7-4abd-8733-3b9a68c375fa',
 secret = 'secret-test-zgxGcqbRPjKEr_MkhBWag2-KSXvaY1MoCKY=',
@@ -72,13 +73,14 @@ exports.handler = async function (event, context) {
 				}
 				break;
 			case "CHART":
+				const data = await fetch('https://astro-api-a4afb1474dd8.herokuapp.com/snapshot?place=macclesfield%20england&year=1983&month=3&day=15&hour=15&minute=35');
 				await client.users.update({
 					user_id:  user_id,
 					name: {
         				first_name: "Maz",
         			},
 					trusted_metadata: {
-						"astrology": {
+						"natal": {
 							"latLng": 1,
 						}
 					}
